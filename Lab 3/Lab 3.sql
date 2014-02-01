ea4044bf5abee2899230e01cd9b8fb83792dae9b
deceptivecadence <michaelmucci64@gmail.com>
2014-02-01 17:35:53 -0500

    Lab 3 complete

---
+++ Lab 3/Lab 3.sql
@@ -0,0 +1,9 @@
+﻿SELECT name, city FROM agents WHERE name = 'Smith';
+SELECT pid, name, quantity FROM products WHERE priceusd > 1.25;
+SELECT ordno, aid FROM orders;
+SELECT name, city FROM customers WHERE city = 'Dallas';
+SELECT name FROM agents WHERE city <> 'New York' AND city <> 'Newark';
+SELECT * FROM products WHERE city <> 'New York' AND city <> 'Newark' AND priceusd >= 1;
+SELECT * FROM orders WHERE mon = 'jan' OR mon = 'mar';
+SELECT * FROM orders WHERE mon = 'feb' AND dollars < 100;
+SELECT * FROM orders WHERE cid = 'c001';
