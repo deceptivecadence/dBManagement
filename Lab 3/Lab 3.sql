e7c7b7e1eacff629dfa8ab58f99169c2a8f6df8c
deceptivecadence <michaelmucci64@gmail.com>
2014-02-04 15:10:51 -0500

    Changed formatting

--- Lab 3/Lab 3.sql
+++ Lab 3/Lab 3.sql
@@ -1,9 +1,43 @@
-﻿SELECT name, city FROM agents WHERE name = 'Smith';
-SELECT pid, name, quantity FROM products WHERE priceusd > 1.25;
-SELECT ordno, aid FROM orders;
-SELECT name, city FROM customers WHERE city = 'Dallas';
-SELECT name FROM agents WHERE city <> 'New York' AND city <> 'Newark';
-SELECT * FROM products WHERE city <> 'New York' AND city <> 'Newark' AND priceusd >= 1;
-SELECT * FROM orders WHERE mon = 'jan' OR mon = 'mar';
-SELECT * FROM orders WHERE mon = 'feb' AND dollars < 100;
-SELECT * FROM orders WHERE cid = 'c001';
+﻿--1
+SELECT name, city
+FROM agents
+WHERE name = 'Smith';
+
+--2
+SELECT pid, name, quantity
+FROM products
+WHERE priceusd > 1.25;
+
+--3
+SELECT ordno, aid
+FROM orders;
+
+--4
+SELECT name, city
+FROM customers
+WHERE city = 'Dallas';
+
+--5
+SELECT name
+FROM agents
+WHERE city <> 'New York' AND city <> 'Newark';
+
+--6
+SELECT *
+FROM products
+WHERE city <> 'New York' AND city <> 'Newark' AND priceusd >= 1;
+
+--7
+SELECT *
+FROM orders
+WHERE mon = 'jan' OR mon = 'mar';
+
+--8
+SELECT *
+FROM orders
+WHERE mon = 'feb' AND dollars < 100;
+
+--9
+SELECT *
+FROM orders
+WHERE cid = 'c001';
