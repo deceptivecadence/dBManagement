ea062ad636c4a48e7d7b3b85de86736e4da99f29
deceptivecadence <michaelmucci64@gmail.com>
2014-02-08 17:55:17 -0500

    need to get clarification on question 2

---
+++ Lab 4/Lab 4 - Mike Mucci.sql
@@ -0,0 +1,64 @@
+﻿--Mike Mucci
+--Spring 2014
+--Lab 4
+--2/8/14
+
+--1
+select city
+from agents
+where aid in (
+		select aid
+		from orders
+		where cid in (
+				select cid
+				from customers
+				where name = 'Basics'))
+
+--2 not finished
+select cid from customers where city = 'Kyoto'
+select * from agents
+select * from orders where cid in (select cid from customers where city = 'Kyoto')
+
+--3
+select cid, name
+from customers
+where cid in (
+		select cid
+		from orders
+		where pid <> 'a03')
+
+--4
+select cid, name
+from customers
+where cid in (
+		select cid
+		from (
+			select cid 
+			from orders
+			where pid = 'p01')
+			as cid
+			where cid in (
+					select distinct cid
+					from orders
+					where pid = 'p07'))
+--5
+select distinct pid from orders where aid = 'a03'
+
+--6
+select name, discount
+from customers
+where cid in (
+		select distinct cid
+		from orders
+		where aid in (
+				select aid
+				from agents
+				where city in ('Duluth','Dallas')))
+
+--7
+select *
+from customers
+where discount in (
+			select discount
+			from customers
+			where city in ('Dallas', 'Kyoto'))
